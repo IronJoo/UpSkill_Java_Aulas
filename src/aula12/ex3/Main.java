d2c95226f0ed75e4eeb100b30688fc3e8e583c4c
IronJoo <joanaferro92@gmail.com>
2022-05-21 20:44:55 +0100

    Teste

--- src/aula12/ex3/Main.java
+++ src/aula12/ex3/Main.java
@@ -27,7 +27,6 @@ public class Main {
         System.out.println(filaSenhas.poll());
         System.out.println(filaSenhas.poll());
         System.out.println(filaSenhas.poll());
-        System.out.println("Teste");
 
 
     }
